96361bc949812276fdc4d62839d65abb40ff26c7
PRASANNA BHALERAO <144560851+prasannabhalerao@users.noreply.github.com>
2024-06-27 21:59:12 +0530

    Add files via upload

---
+++ Rock_Paper_Scissors_Game/script.js
@@ -0,0 +1,72 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset");
+let newgamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let turn = true;
+
+const winpattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+const resetgame = () => {
+    turn = true;
+    enableboxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn === true) {
+            box.innerText = "X";
+            turn = false;
+        } else {
+            box.innerText = "O";
+            turn = true;
+        }
+        box.disabled = true; 
+        checkwinner();
+    });
+});
+
+const disableboxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true; 
+    });
+};
+
+const enableboxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = false; 
+        box.innerText = "";
+    });
+};
+
+const showwinner = (winner) => {
+    msg.innerText = `CONGRATULATIONS, WINNER IS ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableboxes();
+};
+
+const checkwinner = () => {
+    for (let pattern of winpattern) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                showwinner(pos1);
+            }
+        }
+    }
+};
+
+newgamebtn.addEventListener("click", resetgame);
+resetbtn.addEventListener("click", resetgame);
